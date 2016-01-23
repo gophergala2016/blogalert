d968415761477bc14f5f7cb874c5ada837aaead9
Adam Talbot <adam.talbot@fansz.com>
2016-01-23 03:18:50 +0000

    Added method to load blog title from page title

--- blog.go
+++ blog.go
@@ -1,9 +1,38 @@
 package blogalert
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 // Blog defines blog structure
 type Blog struct {
 	URL   string
 	Title string
+}
+
+// LoadBlog creates a blog by parsing the page title of its URL
+func LoadBlog(URL string) (*Blog, error) {
+	res, err := http.Get(URL)
+	defer res.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	doc, err := goquery.NewDocumentFromResponse(res)
+	if err != nil {
+		return nil, err
+	}
+
+	title := doc.Find("title").First().Text()
+
+	if title == "" {
+		return nil, fmt.Errorf("Invalid blog %s - no title", URL)
+	}
+
+	return NewBlog(URL, title), nil
 }
 
 // NewBlog creates a new blog
